cefc6ad28a47ca4675967fa70e8ec20253c40f01
dantereto <2247@holbertonschool.com>
2021-07-19 16:58:42 -0500

    preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+ /**
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: pointer to data structure binary_tree_t
+ * @value: integer with binary tree node value
+ * Return: return binary tree node
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+if (tree != NULL && func != NULL)
+{
+func(tree->n);
+binary_tree_preorder(tree->left, func(tree->left));
+binary_tree_preorder(tree->right, func);
+}
+}
