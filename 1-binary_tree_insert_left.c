ef2eefb51ed594b9190425c1240546de34b196b1
dantereto <2247@holbertonschool.com>
2021-07-19 15:38:32 -0500

    insert a element on the left

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+ /**
+ * binary_tree_node - function that creates a binary tree node
+ * @parent: pointer to data structure binary_tree_t
+ * @value: integer with binary tree node value
+ * Return: return binary tree node
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *new;
+if (parent == NULL)
+{
+return (NULL);
+}
+new = binary_tree_node(parent, value);
+if (parent->left != NULL)
+{
+parent->left->parent = new;
+new->left = parent->left;
+}
+parent->left = new; 
+return (new);
+}
