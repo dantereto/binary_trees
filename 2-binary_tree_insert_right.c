750f326b1c6064546bc7a16131f4f33feb9b2a3b
oangel26 <oa.angel26@gmail.com>
2021-07-19 15:53:58 -0500

    First commit

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+ /**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to data structure binary_tree_t
+ * @value: integer with binary tree node value
+ * Return: return binary tree node
+ */
+
+ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+  /* guard condtion */
+  binary_tree_t *new;
+  if (parent == NULL)
+    {
+      return (NULL);
+    }
+  new = binary_tree_node(parent, value);
+  if (parent->right != NULL)
+    {
+      parent->right->parent = new;
+      new->right = parent->right;
+    }
+  else
+    {
+      parent->right = new;
+    }
+  return (new);
+}
