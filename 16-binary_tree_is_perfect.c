d4ab2c180f21d0f61006c8fe695991fb42b56f2f
oangel26 <oa.angel26@gmail.com>
2021-07-19 21:38:44 -0500

    First commit

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree:  is a pointer to the root node
+ * Return: return 1 if perfect 0 if other
+ */
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+  int height = 0;
+  int factor = 1;
+  int leaves = 0;
+  int i;
+  
+  if(tree == NULL)
+    {
+      return (0);
+    }
+  leaves = binary_tree_leaves(tree);
+  height = binary_tree_height(tree);
+  
+    for (i = 0; i < height; i++)
+      {
+	factor = factor * 2;
+      }
+
+  if (factor == leaves)
+    {
+      return (1);
+    }
+  else
+    return (0);
+}
