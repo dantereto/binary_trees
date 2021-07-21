af22d22de830ba8fd8b069ff90bf278515a8ae96
dantereto <2247@holbertonschool.com>
2021-07-21 03:58:50 -0500

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -36,7 +36,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 int balance = 0;
 int left_hight = 0;
 int right_hight = 0;
-
+if (tree == NULL)
+return (0);
 if (tree->left != NULL)
 {
 	left_hight = binary_tree_height(tree->left) + 1;
