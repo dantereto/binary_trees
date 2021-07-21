554ccc30d0ea91523e92aca360558079af992139
dantereto <2247@holbertonschool.com>
2021-07-21 03:41:03 -0500

    binary uncle

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,7 +9,8 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 if (node == NULL || node->parent->parent == NULL ||
-	node->parent->parent->left == NULL || node->parent->parent->right == NULL)
+	node->parent->parent->left == NULL ||
+	 node->parent->parent->right == NULL || node->parent == NULL)
 {
 return (NULL);
 }
