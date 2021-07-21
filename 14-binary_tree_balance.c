3588a2651dd0182d3cb1bd2401f7ea41082d5f39
Oscar Angel <oa.angel26@gmail.com>
2021-07-21 19:02:50 +0000

    Style correction

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -5,13 +5,17 @@
  * @tree:  is a pointer to the root node
  * Return: return binary tree node
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left_side = 0, right_side = 0;
+
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
+
 	left_side = binary_tree_height(tree->left);
 	right_side = binary_tree_height(tree->right);
+
 	if (left_side > right_side)
 	{
 		return (left_side + 1);
