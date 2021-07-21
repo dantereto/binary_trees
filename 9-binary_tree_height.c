daa37eda1074d114665a1bad596ae13b31bf6304
dantereto <2247@holbertonschool.com>
2021-07-21 00:48:03 -0500

    check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,9 +7,9 @@
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t left_side = 0, right_side = 0;
 if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 return (0);
-size_t left_side = 0, right_side = 0;
 left_side = binary_tree_height(tree->left);
 right_side = binary_tree_height(tree->right);
 if (left_side > right_side)
