8ce34d505cbcca99f5118f8c6b4083bac2412e43
oangel26 <oa.angel26@gmail.com>
2021-07-21 19:59:33 -0500

    Updated

--- binary_trees.h
+++ binary_trees.h
@@ -55,7 +55,7 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 int binary_tree_is_bst(const binary_tree_t *tree);
-bst_t *bst_insert(bst_t **tree, int value);
+/* bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_remove(bst_t *root, int value);
 int binary_tree_is_avl(const binary_tree_t *tree);
@@ -67,7 +67,7 @@ int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
-int *heap_to_sorted_array(heap_t *heap, size_t *size);
+int *heap_to_sorted_array(heap_t *heap, size_t *size); */
 
 
 
