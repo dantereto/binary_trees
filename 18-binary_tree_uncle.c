c16184dbef6ea37cb7de749e4ea3b4548be83a03
oangel26 <oa.angel26@gmail.com>
2021-07-21 19:21:38 -0500

    Son verification added

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -14,14 +14,20 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	if (node->parent->parent->left->n == node->parent->n)
-	{
-		return (node->parent->parent->right);
-	}
-	if (node->parent->parent->right->n == node->parent->n)
+	/* EDGE case parent and ucle with same value */
+	if (node->parent->parent->right->n == node->parent->n &&
+		(node->parent->parent->right->right->n == node->n ||
+		 node->parent->parent->right->left->n == node->n))
 	{
 		return (node->parent->parent->left);
 	}
+	if (node->parent->parent->left->n == node->parent->n &&
+		(node->parent->parent->left->right->n == node->n ||
+          node->parent->parent->left->left->n == node->n))
+	{
+	    return (node->parent->parent->right);
+	}
+
 	else
 		return (NULL);
 }
